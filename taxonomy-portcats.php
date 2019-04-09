86178c798efc2aeb5dbfc7bd6f50861823e8c604
lisadebona <annalisa.quiamco@gmail.com>
2019-04-09 15:24:27 -0600

    Added sub-nav for category page

--- taxonomy-portcats.php
+++ taxonomy-portcats.php
@@ -7,11 +7,18 @@ get_header(); ?>
 
 <div id="primary" class="full-content-area clear">
 	<div id="content">
+		<?php 
+			$obj = get_queried_object(); 
+			$page_title = ( isset($obj->name) && $obj->name ) ? $obj->name : '';
+		?>
+		<h1 class="category-title"><?php echo $page_title; ?></h1>
+
+		<?php get_template_part('inc/tax-subnav-portfolio'); ?>
 
 		<?php  $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 		if ( $term->parent == 0 ) { // if is top level term ?>
 			
-			<?php get_template_part('inc/tax-subnav-tax'); ?>
+			<?php //get_template_part('inc/tax-subnav-tax'); ?>
 
 		<?php } ?>
 
