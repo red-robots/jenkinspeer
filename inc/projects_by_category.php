ca451f8d678ec3d609c94918b9f8e4d3987f7731
lisadebona <annalisa.quiamco@gmail.com>
2019-04-10 13:44:32 -0600

    Updated project list layout

--- inc/projects_by_category.php
+++ inc/projects_by_category.php
@@ -16,23 +16,38 @@ $wp_query->query(array(
 if($wp_query) { ?>
 <div class="three-column-wrapper clear">
     <div class="flexrow clear">
-		<?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-		<div class="flexcol">
-			<div class="inside clear">
-			    <a class="boxlink" href="<?php the_permalink(); ?>?cat=<?php echo $term->term_id; ?>">
-					<?php  if ( has_post_thumbnail() ) { ?>
-			        	<span class="thumbnail"><?php the_post_thumbnail('thumbnail_large'); ?></span>
-			        <?php } else { ?>
-			        	<span class="thumbnail"><img src="<?php bloginfo('template_url'); ?>/images/default-thumb.png" width="130px" height="130px"/></span>
-			        <?php } ?>
-			        <span class="project-info">
-			    		<h2><?php the_title(); ?></h2>
-		   		 		<span class="client"><?php the_field('second_title'); ?></span><!-- client -->
-		    		</span>
-		    	</a>
-	    	</div>
-	 	</div>
-	 <?php endwhile; wp_reset_postdata();  ?>
+		<?php while ($wp_query->have_posts()) : $wp_query->the_post(); 
+        $post_id = get_the_ID();
+        $post_thumbnail_id = get_post_thumbnail_id( $post_id );
+        $taxonomy = 'portcats';
+        $term = get_the_terms($post_id,$taxonomy);
+        $term_id = ($term) ? $term->term_id : '';
+        $pagelink = get_permalink();
+        $imgSrc = wp_get_attachment_image_src($post_thumbnail_id,'large');
+        $img_url = ($imgSrc) ? $imgSrc[0] : get_bloginfo('template_url') . '/images/default-thumb.png';
+        $post_edit_link = get_edit_post_link($post_id);
+        ?>
+        <div id="proj-<?php echo $post_id?>" class="flexcol">
+            <div class="inside clear">
+                <a class="boxlink" href="<?php echo $pagelink; ?>">
+                    <span class="thumbnail" style="background-image:url('<?php echo $img_url?>')">
+                        <?php  if ( has_post_thumbnail() ) { ?>
+                            <?php the_post_thumbnail('large'); ?>
+                        <?php } else { ?>
+                            <img src="<?php bloginfo('template_url'); ?>/images/default-thumb.png" width="130px" height="130px"/>
+                        <?php } ?>
+                    </span>
+                    <span class="project-info">
+                        <h2><?php the_title(); ?></h2>
+                        <span class="client"><?php the_field('second_title'); ?></span><!-- client -->
+                    </span>
+                </a>
+                <?php if ( current_user_can('administrator') ) { ?>
+                    <a class="editpost" href="<?php echo $post_edit_link ?>">Edit</a>
+                <?php } ?>
+            </div>
+        </div>
+        <?php endwhile;  wp_reset_postdata(); ?>
 	</div>
 </div>
 <?php } ?>
