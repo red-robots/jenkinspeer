85cdf992381d23ec76d808b4904f06c74d815079
lisadebona <annalisa.quiamco@gmail.com>
2019-04-15 10:10:07 -0600

    Updated project page

--- inc/featured-projects.php
+++ inc/featured-projects.php
@@ -4,6 +4,7 @@ if ( $featured ) {  ?>
 <div id="featurePortfolio" class="featured-portfolio clear">
 	<div class="image-slider slides">
 	<?php foreach($featured as $p) {  
+			$custom_name = $p['project_custom_name'];
 			$post_id = $p['project_name'];
 			$img = $p['project_image'];
 			$img_bg = ($img) ? ' style="background-image:url('.$img['url'].')"':'';
@@ -13,8 +14,8 @@ if ( $featured ) {  ?>
 			<div class="fp-image-wrap slide-group"<?php echo $img_bg;?>>
 				<div class="fp-caption wrapper">
 					<div class="bottom">
-					<?php if ($first_title) { ?>
-						<h3 class="project-title"><a href="<?php echo $pagelink; ?>"><?php echo $first_title ?> <span class="arrow"><i class="fas fa-chevron-right"></i></span></a></h3>
+					<?php if ($custom_name) { ?>
+						<h3 class="project-title"><a href="<?php echo $pagelink; ?>"><?php echo $custom_name ?> <span class="arrow"><i class="fas fa-chevron-right"></i></span></a></h3>
 					<?php } ?>
 					</div>
 				</div>
