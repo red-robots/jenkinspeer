803c9938c589d9fc2a27fc4669b22c7bbb6d3f4d
Lisa <lisaqdebona@gmail.com>
2020-06-11 02:28:53 -0600

    Modified mission section on homepage

--- front-page.php
+++ front-page.php
@@ -5,19 +5,29 @@ get_header(); ?>
 		<?php get_template_part('inc/project-categories'); ?>
 	</div>
 	<?php while ( have_posts() ) : the_post(); 
-		$col_right = get_field('home_column_right');
-		$col_left = get_field('home_column_left'); ?>
+		$mission_image = get_field('mission_image');
+		$mission_text = get_field('mission_text'); 
+		$row1CLass = ($mission_image && $mission_text) ? 'half':'full';
+		$placeholder = get_bloginfo("template_url") . "/images/rectangle.png";
+		?>
 
-		<?php if ( $col_right || $col_left ) { ?>
-		<section class="home-column-text clear">
-			<div class="wrapper">
+		<?php if ( $mission_image || $mission_text ) { ?>
+		<section class="home-column-text clear <?php echo $row1CLass ?>">
+			<div class="full-wrapper">
 				<div class="flexrow clear">
+					<?php if ($mission_image) { ?>
 					<div class="column right">
-						<div class="inside clear"><?php echo $col_right; ?></div>
+						<div class="inside clear" style="background-image:url('<?php echo $mission_image['url'] ?>')">
+							<img src="<?php echo $placeholder ?>" alt="" aria-hidden="true">
+						</div>
 					</div>
+					<?php } ?>
+					
+					<?php if ($mission_text) { ?>
 					<div class="column left">
-						<div class="inside clear"><?php echo $col_left; ?></div>
+						<div class="inside clear"><?php echo $mission_text; ?></div>
 					</div>
+					<?php } ?>
 				</div>
 			</div>	
 		</section>	
